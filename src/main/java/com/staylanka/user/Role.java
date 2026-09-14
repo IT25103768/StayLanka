6159dea4328d75bb68f2ac9994466e202e60937a
IT25103768 <it25103768@my.sliit.lk>
2026-09-14 15:23:26 +0530

    Add six module-owner roles

--- src/main/java/com/staylanka/user/Role.java
+++ src/main/java/com/staylanka/user/Role.java
@@ -2,7 +2,20 @@ package com.staylanka.user;
 
 public enum Role {
     CUSTOMER,
+    ROOM_MANAGER,
+    RESERVATION_MANAGER,
+    PROFILE_MANAGER,
+    STAY_MANAGER,
+    PROMOTION_REVIEW_MANAGER,
+    INQUIRY_REQUEST_MANAGER,
     STAFF,
-    ADMIN
-}
+    ADMIN;
 
+    public boolean isModuleManager() {
+        return switch (this) {
+            case ROOM_MANAGER, RESERVATION_MANAGER, PROFILE_MANAGER, STAY_MANAGER,
+                    PROMOTION_REVIEW_MANAGER, INQUIRY_REQUEST_MANAGER -> true;
+            default -> false;
+        };
+    }
+}
